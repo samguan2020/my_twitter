e2705b2169812fe566b0b44b0843d4c8f4b651f1
Xin Guan <xin.guan@gmail.com>
2021-04-25 14:37:04 -0700

    login logout signup apis unit tests

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '4tw^h%+-l%dj1nn5+^=jn*5o=mb33mo19vin0x0h$luspli%sl'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
-
+INTERNAL_IPS = ['10.0.2.2']
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'debug_toolbar',
 ]
 
 REST_FRAMEWORK = {
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
