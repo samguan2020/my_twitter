69d2df25082000d8e36bd237eca21cdb8a867a08
cindyzhu77 <68926092+cindyzhu77@users.noreply.github.com>
2021-05-20 21:57:47 -0700

    Get tweet with comments

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from tweets.models import Tweet
 from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
 
 
 class TweetSerializer(serializers.ModelSerializer):
@@ -10,6 +11,19 @@ class TweetSerializer(serializers.ModelSerializer):
         model = Tweet
         fields = ('id', 'user', 'created_at', 'content')
 
+
+class TweetSerializerWithComments(TweetSerializer):
+    # <HOMEWORK> 使用 serialziers.SerializerMethodField 的方式实现 comments
+    comments = serializers.SerializerMethodField()
+
+    # comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'created_at', 'content', 'comments')
+
+    def get_comments(self, obj):
+        return CommentSerializer(obj.comment_set.all(), many=True).data
 
 class TweetSerializerForCreate(serializers.ModelSerializer):
     content = serializers.CharField(min_length=6, max_length=140)
