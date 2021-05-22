468f750c5fb6c94b4cae458949e9f2312823d472
cindyzhu77 <68926092+cindyzhu77@users.noreply.github.com>
2021-05-21 17:04:23 -0700

    Add likes model and admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from utils.time_helpers import utc_now
+from likes.models import Like
+
 
 
 class Tweet(models.Model):
@@ -22,6 +25,15 @@ class Tweet(models.Model):
     def hours_to_now(self):
         # datetime.now 不带时区信息，需要增加上 utc 的时区信息
         return (utc_now() - self.created_at).seconds // 3600
+
+
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
+
 
     def __str__(self):
         return f'{self.created_at} {self.user}: {self.content}'
