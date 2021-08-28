2c9c8db05ae73edff2574e1081b2a1e697c30afb
Sam Guan <xin.guan@gmail.com>
2021-08-28 14:09:03 -0700

    fix again the bugs

--- twitter/cache.py
+++ twitter/cache.py
@@ -1,7 +1,10 @@
 # memcached
 FOLLOWINGS_PATTERN = 'followings:{user_id}'
+<<<<<<< Updated upstream
 USER_PROFILE_PATTERN = 'userprofile:{user_id}'
 
 # redis
 USER_TWEETS_PATTERN = 'user_tweets:{user_id}'
 USER_NEWSFEEDS_PATTERN = 'user_newsfeeds:{user_id}'
+=======
+>>>>>>> Stashed changes
